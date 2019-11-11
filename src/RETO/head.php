d0602bba5ffb3f129d64fb74920f5667605c6fcb
Sergio <sergio.zulueta@ikasle.egibide.org>
2019-11-11 13:11:04 +0100

    head y css

--- src/RETO/head.php
+++ src/RETO/head.php
@@ -2,23 +2,19 @@
 
 ?>
 
-
-<html id="head">
+<html id="html">
 <head>
+
+    <title>Yolo pop</title>
     <link rel="StyleSheet" href="Assets/CSS/main.css" type="text/css">
+    <script src="Assets/JS/Login.js"></script>
+    <nav id="headNav">
+        <ul id="headUl">
+            <li><a href="#">YoloPop</a></li>
+            <li><a href="#">Subir Producto</a></li>
+            <li><a href="Login.php">Login</a></li>
+            <li><a href="#">Salir</a></li>
+        </ul>
+    </nav>
 
 </head>
-<body id="headBody">
-<nav id="headNav">
-
-    <ul id="headUl">
-        <li><a href="#">YoloPop</a></li>
-        <li><a href="#">Subir Producto</a></li>
-        <li><a href="Login.php">Login</a></li>
-        <li><a href="#">Salir</a></li>
-    </ul>
-
-</nav>
-
-</body>
-</html>
