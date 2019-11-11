5218f25082ef58e6109f87c47775887b2ac37e79
Sergio <sergio.zulueta@ikasle.egibide.org>
2019-11-11 11:48:20 +0100

    include

---
+++ vagrant/RETO/head.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+
+<html id="head">
+<head>
+    <link rel="StyleSheet" href="Assets/CSS/main.css" type="text/css">
+
+</head>
+<body id="headBody">
+<nav id="headNav">
+
+    <ul id="headUl">
+        <li><a href="#">YoloPop</a></li>
+        <li><a href="#">Subir Producto</a></li>
+        <li><a href="Login.php">Login</a></li>
+        <li><a href="#">Salir</a></li>
+    </ul>
+
+</nav>
+
+</body>
+</html>
