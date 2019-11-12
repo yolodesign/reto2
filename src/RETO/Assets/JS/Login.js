0ddc2df44c1070c636f252549ab8d6854f4a0d65
Arkaitz <97agalisteo@sanferminikastola.org>
2019-11-12 19:23:02 +0100

    Product class, query y  teléfono

--- src/RETO/Assets/JS/Login.js
+++ src/RETO/Assets/JS/Login.js
@@ -19,6 +19,7 @@ function login() {
 function signup() {
     let expRegCorreo = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
     let expRegPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/
+    let expRegPhone = /^(\+34|0034|34)?[6|7|8|9][0-9]{8}$/;
     let nombre = document.getElementById("signup-name").value;
     let apellido = document.getElementById("signup-lastname").value;
     let cumple = document.getElementById("signup-birthday").value;
@@ -26,38 +27,43 @@ function signup() {
     let email = document.getElementById("signup-email").value;
     let password = document.getElementById("signup-password").value;
     let politicaPriv = document.getElementById("signup-privacidad").checked;
+    let phone = document.getElementById("signup-phone").value;
     let today = new Date();
     if (nombre != "" && apellido != "" && cumple != "" && gender != "" && email != "" && password != "") {
         if (politicaPriv == true) {
-            if (expRegCorreo.exec(email)) {
-                if (expRegPassword.exec(password)) {
+            if (expRegCorreo.exec(phone)){
+                if (expRegCorreo.exec(email)) {
+                    if (expRegPassword.exec(password)) {
+                        let edad;
+                        if (cumple.getMonth == today.getMonth && cumple.getDay == today.getDay) {
+                            edad = today.getFullYear - cumple.getFullYear
+                        } else if (cumple.getMonth > today.getMonth) {
+                            edad = today.getFullYear - cumple.getFullYear - 1
+                        } else if (cumple.getMonth == today.getMonth && cumple.getDay > today.getDay) {
+                            edad = today.getFullYear - cumple.getFullYear - 1
+                        } else if (cumple.getMonth == today.getMonth && cumple.getDay < today.getDay) {
+                            edad = today.getFullYear - cumple.getFullYear
+                        } else if (cumple.getMonth < today.getMonth) {
+                            edad = today.getFullYear - cumple.getFullYear
+                        }
 
-                    let edad;
-                    if (cumple.getMonth == today.getMonth && cumple.getDay == today.getDay) {
-                        edad = today.getFullYear - cumple.getFullYear
-                    } else if (cumple.getMonth > today.getMonth) {
-                        edad = today.getFullYear - cumple.getFullYear - 1
-                    } else if (cumple.getMonth == today.getMonth && cumple.getDay > today.getDay) {
-                        edad = today.getFullYear - cumple.getFullYear - 1
-                    } else if (cumple.getMonth == today.getMonth && cumple.getDay < today.getDay) {
-                        edad = today.getFullYear - cumple.getFullYear
-                    } else if (cumple.getMonth < today.getMonth) {
-                        edad = today.getFullYear - cumple.getFullYear
-                    }
-                        edad = 18;
-                    if (edad >= 18) {
-                        error("Entra.")
-                        return true;
-                    }else{
-                        error("Solo las personas mayores de 18 años pueden registrarse en esta página.")
+                        if (edad >= 18) {
+                            error("Entra.")
+                            return true;
+                        }else{
+                            error("Solo las personas mayores de 18 años pueden registrarse en esta página.")
+                            return false;
+                        }
+                    } else {
+                        error("Por favor, introduzca una contraseña con letras mayúsculas, minúsculas y números.")
                         return false;
                     }
                 } else {
-                    error("Por favor, introduzca una contraseña con letras mayúsculas, minúsculas y números.")
+                    error("Por favor, introduzca un email válido.")
                     return false;
                 }
-            } else {
-                error("Por favor, introduzca un email válido.")
+            }else{
+                error("Por favor, introduzca un teléfono válido.")
                 return false;
             }
         } else {
