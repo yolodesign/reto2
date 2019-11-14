f57c00aa28d3dcbf2a79c7944d8fc9b4ef480db8
Sergio Zulueta <sergio.zulueta@ikasle.egibide.org>
2019-11-15 00:37:34 +0100

    mil arreglos bdd y php, navegacion

---
+++ src/RETO/Assets/JS/navegacionCategorias.js
@@ -0,0 +1,32 @@
+$( () => {
+
+    $(window).scroll( () => {
+        var windowTop = $(window).scrollTop();
+        windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
+        windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
+    });
+
+    $('#logo').on('click', () => {
+        $('html,body').animate({
+            scrollTop: 0
+        },500);
+    });
+
+    $('a[href*="#"]').on('click', function(e){
+        $('html,body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        },500);
+        e.preventDefault();
+    });
+
+    $('#menu-toggle').on('click', () => {
+        $('#menu-toggle').toggleClass('closeMenu');
+        $('ul').toggleClass('showMenu');
+
+        $('li').on('click', () => {
+            $('ul').removeClass('showMenu');
+            $('#menu-toggle').removeClass('closeMenu');
+        });
+    });
+
+});
