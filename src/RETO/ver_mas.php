8af383bfae5ec7203a6b92952ac3b1089bc485de
Casti <julen.castillo@ikasle.egibide.org>
2019-11-14 10:18:27 +0100

    ver mas

---
+++ src/RETO/ver_mas.php
@@ -0,0 +1,32 @@
+<?php
+include "head.php"
+?>
+
+    <section class="contenido">
+        <div class="ver_mas_anuncios">
+                <div class="box item1">One</div>
+                <div class="box item2">
+
+                    <div class="foto_anuncio">
+                        <img src="https://cdn1.iconfinder.com/data/icons/minicons-4/64/box_split_cross-512.png" alt="foto"/>
+                    </div>
+
+                    <div class="descripcion_anuncio">
+                        <h2>Titulo</h2>
+                        <p>Descripcion</p>
+                    </div>
+
+
+
+                </div>
+                <div class="box item3">Three</div>
+                <div class="box item4">Four</div>
+                <div class="box item5">Five</div>
+            </div>
+    </section>
+
+
+
+<?php
+include "footer.php"
+?>
